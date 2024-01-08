d33f4812130021d251105276a7b2b8fb77e73176
11168833 <11168833@H22358>
2024-01-08 14:44:52 +0000

    Add project files.

---
+++ ConverterM/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.txtFrom = New System.Windows.Forms.TextBox()
+        Me.txtTo = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Btn1
+        '
+        Me.Btn1.Font = New System.Drawing.Font("Gloucester MT Extra Condensed", 26.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn1.Location = New System.Drawing.Point(293, 274)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(225, 59)
+        Me.Btn1.TabIndex = 0
+        Me.Btn1.Text = "Convert"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Palace Script MT", 72.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(278, 50)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(260, 89)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Text = "Converter"
+        '
+        'txtFrom
+        '
+        Me.txtFrom.Location = New System.Drawing.Point(293, 178)
+        Me.txtFrom.Name = "txtFrom"
+        Me.txtFrom.Size = New System.Drawing.Size(225, 20)
+        Me.txtFrom.TabIndex = 1
+        '
+        'txtTo
+        '
+        Me.txtTo.Location = New System.Drawing.Point(293, 215)
+        Me.txtTo.Name = "txtTo"
+        Me.txtTo.Size = New System.Drawing.Size(225, 20)
+        Me.txtTo.TabIndex = 3
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(293, 142)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(225, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.txtTo)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.txtFrom)
+        Me.Controls.Add(Me.Btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents txtFrom As TextBox
+    Friend WithEvents txtTo As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ ConverterM/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        ' Define the conversion rates
+        Const DollarToPound As Double = 0.72
+        Const EuroToPound As Double = 0.86
+        Const ZlotyToPound As Double = 0.19
+        Const HryvniaToPound As Double = 0.03
+        Const LeuToPound As Double = 0.18
+
+        ' Get the input value
+        Dim inputValue As Double = Double.Parse(txtFrom.Text)
+
+        ' Convert the input value to Pounds
+        Dim convertedValue As Double
+
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "Dollars"
+                convertedValue = inputValue * DollarToPound
+            Case "Euro"
+                convertedValue = inputValue * EuroToPound
+            Case "Zloty"
+                convertedValue = inputValue * ZlotyToPound
+            Case "Hryvnia"
+                convertedValue = inputValue * HryvniaToPound
+            Case "Leu"
+                convertedValue = inputValue * LeuToPound
+            Case Else
+                MessageBox.Show("Please select a valid currency.")
+                Return
+        End Select
+
+        ' Display the converted value
+        txtTo.Text = convertedValue.ToString("F2") & " £"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.AddRange({"Dollars", "Pounds", "Euro", "Zloty", "Hryvnia", "Leu"})
+    End Sub
+End Class
